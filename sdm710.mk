915d908707e164f93b063f7509d33b2a90460889
Bruno Martins <bgcngm@gmail.com>
2019-10-08 14:56:41 +0100

    sdm710-common: Split RF configs from the main NFC config
    This is now expected with the Q NXP NFC halimp.
    
    Change-Id: Ie58c0cb61441e8bbe22ed100c1067a91a49921d1

--- sdm710.mk
+++ sdm710.mk
@@ -241,6 +241,7 @@ PRODUCT_PACKAGES += \
     Tag
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/nfc/libnfc-nxp_RF.conf:$(TARGET_COPY_OUT_VENDOR)/libnfc-nxp_RF.conf \
     $(LOCAL_PATH)/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
 
 # OMX
