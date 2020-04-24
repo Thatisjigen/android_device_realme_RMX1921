9aa7242046f212f5662f22e0725d7dd3ab766557
Roshan Pius <rpius@google.com>
2020-04-24 14:22:42 -0700

    RMX1921: Import and enable WifiOverlays
    Also, merged PixelWifiCommon overlay contents since it is not feasible
    to maintain a common overlay in the device/ folder structure.
    
    Bug: 148617260
    Test: Device boots up and connects to wifi networks.
    Change-Id: I2b535247305195d5e359400c3cc9020ccc133c0a

--- device.mk
+++ device.mk
@@ -473,6 +473,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.wifi.hostapd@1.1.vendor \
     vendor.qti.hardware.wifi.supplicant@2.0.vendor \
     vendor.qti.hardware.wifi.supplicant@2.1.vendor \
+    WifiOverlay \
     wifi-mac-generator \
     wpa_supplicant \
     wpa_supplicant.conf
