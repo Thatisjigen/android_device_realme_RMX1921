bdad822e281c0dce16e266c89c8e2e0b567ae296
David Sehr <sehr@google.com>
2020-04-25 15:50:40 -0700

    RMX1921: Use dex2oat64 as a vendor prop
    Enable dex2oat64 use on sunfish.
    
    Bug: 153380900
    Test: boot and install an application
    Change-Id: I3e7a6e6e9385ff6564d1a2e6dda004ebb061f095
    (cherry picked from commit 126f03be80f57a8a0411842011152d9381589b78)
    Merged-In: I3e7a6e6e9385ff6564d1a2e6dda004ebb061f095

--- vendor_props.mk
+++ vendor_props.mk
@@ -109,7 +109,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
    dalvik.vm.heapsize=512m \
    dalvik.vm.heaptargetutilization=0.75 \
    dalvik.vm.heapminfree=4m \
-   dalvik.vm.heapmaxfree=16m
+   dalvik.vm.heapmaxfree=16m \
+   dalvik.vm.dex2oat64.enabled=true
 
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
