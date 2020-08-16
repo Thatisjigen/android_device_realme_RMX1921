1d1d568cd12b06396476d65540e678f2a52ff3e4
Harshit Jain <god@hyper-labs.tech>
2020-08-16 19:14:41 +0530

    RMX1921: doze: explicitly register on stk_stk2x2x wakeup for proximity events

--- doze/src/org/lineageos/settings/doze/ProximitySensor.java
+++ doze/src/org/lineageos/settings/doze/ProximitySensor.java
@@ -47,7 +47,7 @@ public class ProximitySensor implements SensorEventListener {
     public ProximitySensor(Context context) {
         mContext = context;
         mSensorManager = mContext.getSystemService(SensorManager.class);
-        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+        mSensor = mSensorManager.getDefaultSensor(33171005, true);
         mExecutorService = Executors.newSingleThreadExecutor();
     }
 
