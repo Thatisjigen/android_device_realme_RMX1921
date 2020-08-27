e6f1cb7ae0f1d58598c57367cca4b15d782338b0
Harshit Jain <god@hyper-labs.tech>
2020-08-27 18:01:50 +0530

    RMX1921: sync display flags and offsets with sunfish

--- vendor_props.mk
+++ vendor_props.mk
@@ -125,7 +125,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.egl.hw=0 \
     debug.sf.hw=0 \
-    ro.opengles.version=196610
+    ro.opengles.version=196610 \
+    debug.sf.enable_hwc_vds=1 \
+    dev.pm.dyn_samplingrate=1 \
+    persist.front.skipdisplay.enable=1
 
 # IMS
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -142,11 +145,28 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Surfaceflinger properties
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    ro.surface_flinger.has_wide_color_display=true \
-    ro.surface_flinger.has_HDR_display=true \
-    ro.surface_flinger.use_color_management=true \
     ro.surface_flinger.wcg_composition_dataspace=143261696 \
     ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
+    ro.surface_flinger.max_virtual_display_dimension=4096 \
+    debug.sf.latch_unsignaled=1 \
+    debug.sf.enable_gl_backpressure=1 \
+    ro.surface_flinger.has_wide_color_display=true \
+    ro.surface_flinger.use_color_management=true \
+    ro.surface_flinger.has_HDR_display=true \
     ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
-    ro.surface_flinger.protected_contents=true \
-    ro.surface_flinger.max_virtual_display_dimension=4096
+    ro.surface_flinger.protected_contents=true
+
+# Display post-processing
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.display.ad=1 \
+    ro.vendor.display.sensortype=2 \
+    ro.vendor.display.ad.sdr_calib_data=/vendor/etc/OPPO_OLED_AD_calib.cfg
+
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+   ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
+   ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000 \
+   debug.sf.early_phase_offset_ns=11600000 \
+   debug.sf.early_app_phase_offset_ns=11600000 \
+   debug.sf.early_gl_phase_offset_ns=3000000 \
+   debug.sf.early_gl_app_phase_offset_ns=15000000 \
+   debug.sf.phase_offset_threshold_for_next_vsync_ns=11600000
