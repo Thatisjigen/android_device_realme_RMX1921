be50f3908c6f604ea05e23a7aa4d3ddd24113841
LuK1337 <priv.luk@gmail.com>
2020-09-18 13:12:09 +0200

    RMX1921: Drop android.hardware.thermal HAL
    android.hardware.thermal@1.0::IThermal/default is deprecated in compatibility matrix at FCM Version 4; it should not be served.
    
    Change-Id: Icc3c672a0bf6b9e4ee76ccf91e6cdca5fb778fde

--- device.mk
+++ device.mk
@@ -420,12 +420,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     textclassifier.bundle1
 
-# Thermal
-PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service \
-    thermal.sdm710
-
 # Touch
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.xt
