4864ebe2eebae35d02bbd1fbc0b7fe1520bd7d8d
jjpprrrr <jjpprrrr@pixelexperience.org>
2020-12-01 06:53:19 +0000

    RMX1921: Bump up health hal to 2.1
    Signed-off-by: improbir <ghosh_probir@yahoo.com>

--- device.mk
+++ device.mk
@@ -194,8 +194,9 @@ PRODUCT_USES_QCOM_HARDWARE := true
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.0-impl \
-    android.hardware.health@2.0-service
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-impl.recovery \
+    android.hardware.health@2.1-service
 
 # HIDL
 PRODUCT_PACKAGES += \
