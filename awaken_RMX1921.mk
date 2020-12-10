57be4394b0a600e5318c2d3506e92200e2717e61
L <55486333+Thatisjigen@users.noreply.github.com>
2020-12-10 17:45:30 +0000

    RMX1921: update coral fp to december sp

--- awaken_RMX1921.mk
+++ awaken_RMX1921.mk
@@ -19,8 +19,8 @@ PRODUCT_MODEL := Realme XT
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-     PRIVATE_BUILD_DESC="coral-user 11 RP1A.200720.009 6720564 release-keys" \
+     PRIVATE_BUILD_DESC="coral-user 11 RP1A.201205.008 6943376 release-keys" \
      PRODUCT_NAME="RMX1921"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := google/coral/coral:11/RP1A.200720.009/6720564:user/release-keys
+BUILD_FINGERPRINT := google/coral/coral:11/RP1A.201205.008/6943376:user/release-keys
