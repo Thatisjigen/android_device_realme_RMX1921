f6e13412912e681557ce7c577693ed336be4fb29
improbir <ghosh_probir@yahoo.com>
2020-12-16 18:56:50 +0000

    RMX1921: Add proximity & light sensor permissions

--- device.mk
+++ device.mk
@@ -416,6 +416,11 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-service.xt \
     libsensorndkbridge
 
+# Sensors
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
