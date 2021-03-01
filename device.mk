abd970f8a86d17e526de69c49115a65543b55a02
ThatisJigen <gianfranco.liguori.96@gmail.com>
2021-03-01 20:25:45 +0100

    RMX1921: nuke Snap and switch to Google Camera go

--- device.mk
+++ device.mk
@@ -109,7 +109,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
     libstdc++.vendor \
-    Snap \
+    GoogleCamera \
     vendor.qti.hardware.camera.device@1.0.vendor \
     libgui_vendor
 
@@ -288,7 +288,6 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay/packages/apps/Snap \
     $(LOCAL_PATH)/overlay/frameworks/base/packages/SystemUI
 
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
