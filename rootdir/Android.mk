667c1126815ce4b06284c577477367461b871792
Thatisjigen <gianfranco.liguori.96@gmail.com>
2021-03-31 14:19:58 +0000

    RMX1921: GalaxyParts->RealmeParts *Removed doze from it *Removed gain from it

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -9,17 +9,9 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := init.GalaxyParts.sh
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES := bin/init.GalaxyParts.sh
-LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_EXECUTABLES)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := init.GalaxyParts.rc
+LOCAL_MODULE       := init.RealmeParts.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.GalaxyParts.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/init
+LOCAL_SRC_FILES    := etc/init.RealmeParts.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
