e1c95c6d3d94df6050cd31f3c8624762c34deb89
ThatisJigen <gianfranco.liguori.96@gmail.com>
2021-05-18 18:46:30 +0200

    RMX1921: drop livedisplay

--- device.mk
+++ device.mk
@@ -240,10 +240,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.xt
 
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
-
 # Media
 PRODUCT_PACKAGES += \
     libminijail \
