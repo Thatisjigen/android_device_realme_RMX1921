bc2e46866731fc3291d57740920bc333795fb04e
Omkar Chandorkar <gotenksIN@aosip.dev>
2021-08-03 21:18:02 +0000

    RMX1921: libinit: handle dalvik props based on sysram
    * Adapted to Realme XT.
    * We have 3 variants(4gb, 6gb & 8gb RAM)
    * Values are taken from frameworks/native

--- vendor_props.mk
+++ vendor_props.mk
@@ -125,12 +125,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Dalvik
 PRODUCT_PROPERTY_OVERRIDES += \
-   dalvik.vm.heapstartsize=16m \
-   dalvik.vm.heapgrowthlimit=384m \
-   dalvik.vm.heapsize=512m \
-   dalvik.vm.heaptargetutilization=0.75 \
-   dalvik.vm.heapminfree=4m \
-   dalvik.vm.heapmaxfree=16m \
    dalvik.vm.dex2oat64.enabled=true
 
 # Dirac algo tsx 9/12
