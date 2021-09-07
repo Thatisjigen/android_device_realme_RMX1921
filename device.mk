30fcbfdc02e7392b9b1f8070648f6df9d2323f75
improbir <ghosh_probir@yahoo.com>
2021-09-07 19:24:53 +0200

    RMX1921: downrev health profile

--- device.mk
+++ device.mk
@@ -236,9 +236,7 @@ PRODUCT_USES_QCOM_HARDWARE := true
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl \
-    android.hardware.health@2.1-impl.recovery \
-    android.hardware.health@2.1-service
+    android.hardware.health@2.0-service
 
 # HIDL
 PRODUCT_PACKAGES += \
