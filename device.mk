4267d66c703b60b7f54c5364b5bbd541d187dcc6
SebaUbuntu <barezzisebastiano@gmail.com>
2021-10-22 19:26:48 +0200

    RMX1921: Uprev bluetooth audio HIDL impl to 2.1
    * We get free LC3 support, isn't it poggers?

--- device.mk
+++ device.mk
@@ -33,7 +33,7 @@ PRODUCT_PACKAGES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0 \
-    android.hardware.bluetooth.audio@2.0-impl \
+    android.hardware.bluetooth.audio@2.1-impl \
     audio.bluetooth.default \
     libbthost_if \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
