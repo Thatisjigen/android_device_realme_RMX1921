23cfb3d48c99ebd6ef1577ee1c17403f25dcd1d1
ThatisJigen <gianfranco.liguori.96@gmail.com>
2021-10-28 22:22:19 +0200

    RMX1921: doze: add a service to write and read the proximity status *will be used as a trigger for screen off fod: near: fod disabled far: fod enabled *add a toggle to enable/disable proximity checks for fod

--- doze/src/org/lineageos/settings/doze/DozeSettingsFragment.java
+++ doze/src/org/lineageos/settings/doze/DozeSettingsFragment.java
@@ -53,6 +53,7 @@ public class DozeSettingsFragment extends PreferenceFragment implements OnPrefer
     private SwitchPreference mRaiseToWakePreference;
     private SwitchPreference mPocketPreference;
     private SwitchPreference mSmartWakePreference;
+    private SwitchPreference mFODProxPreference;
 
     private Handler mHandler = new Handler();
 
@@ -99,6 +100,10 @@ public class DozeSettingsFragment extends PreferenceFragment implements OnPrefer
         mSmartWakePreference.setEnabled(dozeEnabled);
         mSmartWakePreference.setOnPreferenceChangeListener(this);
 
+        mFODProxPreference = (SwitchPreference) findPreference(DozeUtils.GESTURE_FOD_PROX_KEY);
+        mFODProxPreference.setEnabled(dozeEnabled);
+        mFODProxPreference.setOnPreferenceChangeListener(this);
+
         // Hide AOD if not supported and set all its dependents otherwise
         if (!DozeUtils.alwaysOnDisplayAvailable(getActivity())) {
             getPreferenceScreen().removePreference(mAlwaysOnDisplayPreference);
@@ -108,6 +113,7 @@ public class DozeSettingsFragment extends PreferenceFragment implements OnPrefer
             proximitySensorCategory.setDependency(DozeUtils.ALWAYS_ON_DISPLAY);
             raiseToWakeGesture.setDependency(DozeUtils.ALWAYS_ON_DISPLAY);
             mSmartWakePreference.setDependency(DozeUtils.GESTURE_PICK_UP_KEY);
+            mFODProxPreference.setDependency(DozeUtils.ALWAYS_ON_DISPLAY);
         }
     }
 
