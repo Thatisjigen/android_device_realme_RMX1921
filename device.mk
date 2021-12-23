f71a7883715e35cf09fa67913af1549c4e56cb5e
MadhavSaladi <baaswanthmadhav@gmail.com>
2021-12-23 15:49:35 +0100

    RMX1921: Build missing libldacBT_bco from source
    E a2dp_vendor_ldac_decoder: A2DP_VendorLoadDecoderLdac: cannot open LDAC decoder library libldacBT_bco.so: dlopen failed: library libldacBT_bco.so not found
    
    Signed-off-by: MadhavSaladi <baaswanthmadhav@gmail.com>

--- device.mk
+++ device.mk
@@ -36,6 +36,7 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.1-impl \
     audio.bluetooth.default \
     libbthost_if \
+    libldacBT_bco \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor
 
