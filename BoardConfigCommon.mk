c400e5c0493571f17270ed4ec4e43fb5bf4f98ea
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-02-09 10:06:45 +0100

    sdm710-common: Initial skeleton
    Change-Id: I716e3df116ebbccd0accfb8bc20500ed1e40df69

---
+++ BoardConfigCommon.mk
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+COMMON_PATH := device/xiaomi/sdm710-common
+
+# Inherit the proprietary files
+include vendor/xiaomi/sdm710-common/BoardConfigVendor.mk
---
+++ README.md
@@ -0,0 +1,9 @@
+# Common device tree for Xiaomi SDM710 devices
+
+```
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
