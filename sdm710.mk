17d6f348aff0afc398ab22ac8af33b05cebeab1e
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-02-09 15:50:53 +0100

    sdm710-common: Build Xiaomi light AIDL
    Change-Id: I5a7796e9e34f2edab79d3243634647d7da2b936e

--- sdm710.mk
+++ sdm710.mk
@@ -143,6 +143,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1.vendor
 
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.light-service.xiaomi
+
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
