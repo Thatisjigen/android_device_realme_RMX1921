c86f7f82c0e5c8efe63447694b7cbddd055c9285
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-02-09 21:18:32 +0100

    sdm710-common: Import public libraries list
    * From grus-user 11 RKQ1.20082.002 V12.5.1.0.RFBMIXM release-keys
    
    Change-Id: I13eba92823c176b65bd1ae1d9a3ed847c0c46fe7

--- sdm710.mk
+++ sdm710.mk
@@ -208,6 +208,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti
 
+# Public libraries
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
+
 # QMI
 PRODUCT_PACKAGES += \
     libjson \
