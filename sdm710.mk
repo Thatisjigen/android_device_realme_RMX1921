91bac18e74429d9ff51d2b548472168f21c4b71b
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-02-09 12:31:45 +0100

    sdm710-common: Build audio stack
    Change-Id: Ida0e83f5c542ca59cf8cbc18ba6bf5c334f92034

--- sdm710.mk
+++ sdm710.mk
@@ -10,6 +10,30 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Include GSI keys
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
+# Audio
+PRODUCT_PACKAGES += \
+    android.hardware.audio@6.0-impl \
+    android.hardware.audio.effect@6.0-impl \
+    android.hardware.audio.service \
+    android.hardware.bluetooth.audio@2.0-impl \
+    android.hardware.soundtrigger@2.1-impl
+
+PRODUCT_PACKAGES += \
+    audio.a2dp.default \
+    audio.bluetooth.default \
+    audio.primary.sdm710 \
+    audio.r_submix.default \
+    audio.usb.default
+
+PRODUCT_PACKAGES += \
+    libaudioalsa \
+    libqcompostprocbundle \
+    libqcomvisualizer \
+    libqcomvoiceprocessing \
+    libtinycompress \
+    libvolumelistener \
+    tinymix
+
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
