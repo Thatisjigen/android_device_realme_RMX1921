bd4e03c5609225c64de92159d3dc37271da7a850
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-02-09 14:43:09 +0100

    sdm710-common: Add audio configs
    * From grus-user 11 RKQ1.20082.002 V12.5.1.0.RFBMIXM release-keys
    
    Change-Id: I512f924fab709fddf177953038a2856db5dbe8fa

--- sdm710.mk
+++ sdm710.mk
@@ -34,6 +34,15 @@ PRODUCT_PACKAGES += \
     libvolumelistener \
     tinymix
 
+# Audio configs
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/audio/,$(TARGET_COPY_OUT_VENDOR)/etc)
+
+PRODUCT_COPY_FILES += \
+    frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
+    frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
+    frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml
+
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
