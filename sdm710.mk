279b2f7f24da8bc507dbe09396503fb67f736d20
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-02-09 16:10:45 +0100

    sdm710-common: Build telephony libs
    Change-Id: I0253b8b2daa9935a3d6e80f23cf87e598ebe558d

--- sdm710.mk
+++ sdm710.mk
@@ -233,5 +233,18 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/xiaomi
 
+# Telephony
+PRODUCT_PACKAGES += \
+    ims-ext-common \
+    ims_ext_common.xml \
+    qti-telephony-hidl-wrapper \
+    qti_telephony_hidl_wrapper.xml \
+    qti-telephony-utils \
+    qti_telephony_utils.xml \
+    telephony-ext
+
+PRODUCT_BOOT_JARS += \
+    telephony-ext
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/sdm710-common/sdm710-common-vendor.mk)
