56976c5f8b81b273fe8f51563320be3d11b1cb64
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-02-09 15:13:55 +0100

    sdm710-common: Build camera stack
    Change-Id: I0a1bcb17700f9a944b7bec5b59e29671ffe9e175

--- sdm710.mk
+++ sdm710.mk
@@ -50,6 +50,19 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
+# Camera
+PRODUCT_PACKAGES += \
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service
+
+PRODUCT_PACKAGES += \
+    libcamera2ndk_vendor \
+    libdng_sdk.vendor \
+    libgui_vendor \
+    libstdc++.vendor \
+    vendor.qti.hardware.camera.device@1.0.vendor \
+    vendor.qti.hardware.camera.postproc@1.0.vendor
+
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
