4df84dff1ba76038df2d5e024866d52e787e5c5e
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-02-09 14:55:39 +0100

    sdm710-common: Build Bluetooth libs
    Change-Id: I1cb6451c8cb81bcde21f77b7432beed0263f8e45

--- sdm710.mk
+++ sdm710.mk
@@ -43,6 +43,13 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.1.vendor \
+    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
+    vendor.qti.hardware.btconfigstore@1.0.vendor \
+    vendor.qti.hardware.btconfigstore@2.0.vendor
+
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
