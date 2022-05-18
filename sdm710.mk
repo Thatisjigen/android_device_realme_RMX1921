e29063fcf3b5ec4b21dc69bbb325062a5031e630
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-05-18 22:35:08 +0200

    sdm710-common: rootdir: Cleanup
    * Remove unused or disabled shell scripts
    * Remove init.qcom.factory.rc, it is used for debugging
    
    Change-Id: I5495fccd63eb857e3cd7101c17809aa95f9e0b9c

--- sdm710.mk
+++ sdm710.mk
@@ -321,24 +321,17 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     init.class_main.sh \
-    init.crda.sh \
-    init.mdm.sh \
     init.mi.usb.sh \
     init.qcom.class_core.sh \
-    init.qcom.coex.sh \
     init.qcom.early_boot.sh \
-    init.qcom.efs.sync.sh \
     init.qcom.post_boot.sh \
-    init.qcom.sdio.sh \
     init.qcom.sh \
     init.qcom.usb.sh
 
 PRODUCT_PACKAGES += \
     init.fingerprint.rc \
-    init.qcom.factory.rc \
     init.qcom.rc \
     init.qcom.usb.rc \
-    init.qti.ufs.rc \
     init.recovery.qcom.rc \
     init.target.rc \
     ueventd.qcom.rc
