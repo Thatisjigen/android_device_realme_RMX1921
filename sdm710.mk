841c59904569e9438a8c72612fd22fecd4e90fd5
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-05-19 12:54:20 +0200

    sdm710-common: Import powerhint from sdm845-common
    * https://github.com/LineageOS/android_device_xiaomi_sdm845-common/commit/28089d58b7ffd637069e2a53f38ecd1c4fa1e5a8
    
    Change-Id: Ic38bf29e552d1639ad015481df9f70c11a82700b

--- sdm710.mk
+++ sdm710.mk
@@ -287,6 +287,9 @@ PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay/packages/apps/Car
 PRODUCT_PACKAGES += \
     android.hardware.power-service.xiaomi-libperfmgr
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
 PRODUCT_SOONG_NAMESPACES += \
     hardware/google/interfaces \
     hardware/google/pixel
