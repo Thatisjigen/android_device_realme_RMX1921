1011e824b4b002c43395bf41eeb218cd9719129d
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-05-24 16:36:08 +0200

    sdm710-common: Define DT2W node for power HAL
    Change-Id: I1dc2f23e0c7764a8ca9d5bf6c9f2f78dcd89dbbe

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -96,6 +96,9 @@ BOARD_VENDOR := xiaomi
 BOARD_USES_QCOM_HARDWARE := true
 TARGET_BOARD_PLATFORM := sdm710
 
+# Power
+TARGET_TAP_TO_WAKE_NODE := "/sys/touchpanel/double_tap"
+
 # Properties
 TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
 TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
