0a2d6027bb015828dde6572ec1f5f573faae808a
pjgowtham <pjgowtham@gmail.com>
2022-07-21 15:14:40 +0530

    RMX1921: Use qti vibrator aidl hal

--- device.mk
+++ device.mk
@@ -382,8 +382,10 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl \
-    android.hardware.vibrator@1.0-service
+    vendor.qti.hardware.vibrator.service \
+
+PRODUCT_COPY_FILES += \
+    vendor/qcom/opensource/vibrator/excluded-input-devices.xml:vendor/etc/excluded-input-devices.xml
 
 # Wi-Fi
 PRODUCT_PACKAGES += \
