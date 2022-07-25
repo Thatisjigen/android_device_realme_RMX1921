a61b29f990a0bb537ef8452f94bfe04b71dd693e
Blaster4385 <venkateshchaturvedi12@gmail.com>
2022-07-25 16:53:12 +0530

    RMX1921: Build 64 bit camera service just as stock does

--- device.mk
+++ device.mk
@@ -133,7 +133,7 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service
+    android.hardware.camera.provider@2.4-service_64
 
 PRODUCT_PACKAGES += \
     libcamera2ndk_vendor \
