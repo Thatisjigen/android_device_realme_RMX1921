360842558866bb161490249242f718d29fd5c17c
Thatisjigen <gianfranco.liguori.96@gmail.com>
2022-11-02 11:59:34 +0100

    RMX1921: drop GCam GO

--- device.mk
+++ device.mk
@@ -135,8 +135,7 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service_64 \
-    GoogleCamera
+    android.hardware.camera.provider@2.4-service_64
 
 PRODUCT_PACKAGES += \
     libcamera2ndk_vendor \
